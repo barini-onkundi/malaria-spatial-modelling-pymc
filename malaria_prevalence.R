b9d848e5a6c4ccada9a0fb750a4583dec799f341
Geoffrey Barini <52955693+barini-onkundi@users.noreply.github.com>
2025-05-22 07:35:09 +0300

    Add files via upload

---
+++ malaria_prevalence.R
@@ -0,0 +1,28 @@
+#---install malaria atlas package
+install.packages("malariaAtlas") 
+
+#---required libraries
+library(malariaAtlas)
+library(readr)
+library(dplyr)
+
+#---download prevalence data
+kenya_pfpr <- getPR(country = "Kenya", species = "Pf")
+
+#---view data
+head(kenya_pfpr)
+
+kenya_pfpr<-kenya_pfpr%>%select(longitude, latitude,
+
+                                positive, examined, pr)
+
+#--rename columns
+
+kenya_pfpr<-kenya_pfpr%>%rename(
+  total_positive=positive,
+  total_tested=examined,
+  prevalence= pr
+)
+
+#---write to csv file
+write.csv(kenya_pfpr, "pr_data.csv", row.names = FALSE)
